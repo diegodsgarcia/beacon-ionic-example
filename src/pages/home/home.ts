99740345ddeb716ffbf6d9eea74f1915fd97df36
diegodsgarcia <diego.ds.garcia@gmail.com>
2018-08-25 20:39:00 -0300

    Init project

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+declare var evothings: any;
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
+
+  }
+
+  escanearBeacons() {
 
   }
 
